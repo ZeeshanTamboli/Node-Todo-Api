0b3e5020a82c16a3af0a4a3225d49161e968cd84
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-03-10 12:14:36 +0530

    Added validation and defaults

--- app.js
+++ app.js
@@ -5,34 +5,55 @@ mongoose
   .then(() => console.log('MongoDB connected....'))
   .catch(err => console.log(err));
 
-const Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
+// const Todo = mongoose.model('Todo', {
+//   text: {
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   },
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   },
+//   completedAt: {
+//     type: Number,
+//     default: null
+//   }
+// });
+
+// // const newTodo = new Todo({
+// //   text: 'Cook dinner'
+// // });
+
+// // newTodo
+// //   .save()
+// //   .then(doc => console.log(`Saved todo: ${doc}`))
+// //   .catch(err => console.log('err'));
+
+// const otherTodo = new Todo({
+//   text: true
+// });
+
+// otherTodo
+//   .save()
+//   .then(doc => console.log(`Saved todo: ${doc}`))
+//   .catch(err => console.log(err));
+
+const Users = mongoose.model('Users', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
   }
 });
 
-// const newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-
-// newTodo
-//   .save()
-//   .then(doc => console.log(`Saved todo: ${doc}`))
-//   .catch(err => console.log('err'));
-
-const otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123
+const newUser = new Users({
+  email: ' zeeshan@gmail.com  '
 });
 
-otherTodo
+newUser
   .save()
-  .then(doc => console.log(`Saved todo: ${doc}`))
-  .catch(err => console.log('err'));
+  .then(user => console.log(`Saved user is: ${user}`))
+  .catch(err => console.log(err));
