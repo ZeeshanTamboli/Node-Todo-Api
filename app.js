03f266b34f02ef1d8bed8618f7a1ee4b0598f54f
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-03-17 19:55:24 +0530

    Hashing tokens explanation

--- app.js
+++ app.js
@@ -101,6 +101,18 @@ app.patch('/todos/:id', (req, res) => {
     .catch(e => res.status(400).send());
 });
 
+//POST /users
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  new User(body)
+    .save()
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => res.status(400).send(err));
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
